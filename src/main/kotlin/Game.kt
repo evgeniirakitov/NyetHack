a9c95521bd8056c433d9bea2c0a5e9459004849f
evgeniirakitov <evgenii_rakitov@epam.com>
2021-10-17 01:30:24 +0300

    First commit

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,37 @@
+fun main() {
+
+    val name = "Madrigal"
+    var healthPoints = 100
+    healthPoints -= 15
+    val isBlessed = true
+    val isImmortal = false
+
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+    println(
+        "Aura: $auraColor\n" +
+                "Blessed: ${if (isBlessed) "YES" else "NO"}"
+    )
+    println("$name $healthStatus")
+
+    val race = "gnome"
+    val faction = when (race) {
+        "dwarf" -> "Keepers of the Mines"
+        "gnome" -> "Keepers of the Mines"
+        "ord" -> "Free People of the Rolling Hills"
+        "human" -> "Free People of the Rolling Hills"
+        else -> "Free People of the Rolling Hills"
+    }
+}
